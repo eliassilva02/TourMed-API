36e1e0e9a6fc9703b4383f6e1a33b78ac73b22fa
Elias Silva <eduardoelias14@hotmail.com>
2023-07-07 14:42:53 -0300

    Vs 0.0.5-b1

--- lion.py
+++ lion.py
@@ -28,7 +28,7 @@ def Anvisa(tipo_req, med):
             cur.execute("SELECT public.precos_maximos.ean, public.precos_maximos.nome, public.precos_maximos.principio_ativo, public.precos_maximos.categoria, public.precos_maximos.lista, public.cest.cest FROM public.precos_maximos LEFT JOIN public.cest ON public.precos_maximos.id_cest = public.cest.id_cest WHERE ean = %s;" % (produto))
             rows = cur.fetchall()
             if len(rows) == 0:
-                continue
+                return 'Item não encontrado'
             else:
                 ean = []
                 desc = []
@@ -67,11 +67,12 @@ def Anvisa(tipo_req, med):
 
         elif tipo_req == 'Descrição':
             # Executando uma consulta
-            cur.execute("SELECT public.precos_maximos.ean, public.precos_maximos.nome, public.precos_maximos.principio_ativo, public.precos_maximos.categoria, public.precos_maximos.lista, public.cest.cest FROM public.precos_maximos LEFT JOIN public.cest ON public.precos_maximos.id_cest = public.cest.id_cest WHERE public.precos_maximos.nome ILIKE '%s' LIMIT 1;" % (produto))
+            produto = produto.upper()
+            cur.execute("SELECT public.precos_maximos.ean, public.precos_maximos.nome, public.precos_maximos.principio_ativo, public.precos_maximos.categoria, public.precos_maximos.lista, public.cest.cest FROM public.precos_maximos LEFT JOIN public.cest ON public.precos_maximos.id_cest = public.cest.id_cest WHERE public.precos_maximos.nome = '%s' LIMIT 1;" % (produto))
             rowAnv = cur.fetchall()
             if len(rowAnv) == 0:
                 cur.execute(
-                    "SELECT * FROM public.smerp_meds WHERE nome_smerp ILIKE '%s' LIMIT 1;" % (produto))
+                    "SELECT * FROM public.smerp_meds WHERE nome_smerp = '%s' LIMIT 1;" % (produto))
                 rowsmerp = cur.fetchall()
 
                 if len(rowsmerp) == 0:
@@ -150,108 +151,119 @@ def Anvisa(tipo_req, med):
 # ARRUMAR CONSULTA EM LOTE COM DESCRICAO
 
 def Pesquisa(med):
-    if isinstance(med[0], int):
+    if isinstance(med, int):
         return 'Item não encontrado'
     else:
+        for produto in med:
+            options = webdriver.ChromeOptions()
+            options.add_argument("--headless")
+            options.add_experimental_option(
+                'excludeSwitches', ['enable-logging'])
+            driver = webdriver.Chrome(options=options)
 
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        driver = webdriver.Chrome(options=options)
+            url = 'https://www.smerp.com.br/anvisa/?ac=prodSearch&page=1&fastSearch=&anvisaType=0&anvisaId=&anvisaIdType=0&anvisaProcN=&anvisaProcNType=0&anvisaProdDesc=ultracler&anvisaProdDescType=0&anvisaProdCat=&anvisaProdCatType=0&anvisaProdMod=&anvisaProdModType=0&anvisaHolderReg=&anvisaHolderRegType=0&anvisaProdOrig=&anvisaProdOrigType=0#results'
+            driver.get(url)
 
-        url = 'https://www.smerp.com.br/anvisa/?ac=prodSearch&page=1&fastSearch=&anvisaType=0&anvisaId=&anvisaIdType=0&anvisaProcN=&anvisaProcNType=0&anvisaProdDesc=ultracler&anvisaProdDescType=0&anvisaProdCat=&anvisaProdCatType=0&anvisaProdMod=&anvisaProdModType=0&anvisaHolderReg=&anvisaHolderRegType=0&anvisaProdOrig=&anvisaProdOrigType=0#results'
-        driver.get(url)
+            login = driver.find_element('name', 'anvisaProdDesc')
+            login.clear()
+            login.send_keys("%s" % (produto))
+            tipo = driver.find_element('name', 'anvisaProdDescType')
+            tipo = Select(tipo).select_by_value('1')
 
-        login = driver.find_element('name', 'anvisaProdDesc')
-        login.clear()
-        login.send_keys("%s" % (med[0]))
-        tipo = driver.find_element('name', 'anvisaProdDescType')
-        tipo = Select(tipo).select_by_value('1')
+            btn = driver.find_element(By.ID, 'submitSearch')
+            btn.click()
 
-        btn = driver.find_element(By.ID, 'submitSearch')
-        btn.click()
-
-        try:
-            driver.find_element(By.CLASS_NAME, 'error')
-            abort(404, 'Item não encontrado')
-
-        except:
-            tr_tags = driver.find_elements(By.TAG_NAME, 'tr')
-
-            # Variáveis para armazenar a descrição e o link
-            descricao = None
-            link = None
-
-            # Percorre as tags <tr> e verifica se contêm a descrição desejada
-            for tr in tr_tags:
-                td_tags = tr.find_elements(By.TAG_NAME, 'td')
-                if len(td_tags) >= 2 and 'MG' in td_tags[1].text or len(td_tags) >= 2 and 'MCG' in td_tags[1].text or len(td_tags) >= 2 and 'MG/G' in td_tags[1].text and '165MG/ML' in td_tags[1].text:
-                    descricao = td_tags[1].text
-                    link = tr.find_element(By.XPATH, './/a')
-                    break
-
-            # Clica no link se encontrado
-            if link:
-                link.click()
             try:
-                registro = driver.find_element(
-                    By.XPATH, '//*[@id="anvisa"]/div[1]/div[2]/div[2]').text
-                nome = driver.find_element(
-                    By.XPATH, '//*[@id="anvisa"]/div[1]/div[3]/div[2]').text
-                p_ativo = driver.find_element(
-                    By.XPATH, '//*[@id="anvisa"]/div[1]/div[9]/div[2]').text
-                categoria = driver.find_element(
-                    By.XPATH, '//*[@id="anvisa"]/div[1]/div[10]/div[2]').text
-                lista = driver.find_element(
-                    By.XPATH, '//*[@id="anvisa"]/div[1]/div[25]/div[2]').text
-                lista_ = ''
-                cat_ = ''
+                driver.find_element(By.CLASS_NAME, 'error')
+                abort(404, 'Item não encontrado')
 
-                if categoria != 'Novo' and categoria != 'Similar' and categoria != 'Genérico' and categoria != 'Generico':
-                    cat_ = 'Outros'
-                else:
-                    cat_ = categoria
+            except:
+                tr_tags = driver.find_elements(By.TAG_NAME, 'tr')
 
-                if 'Venda Sob' in lista or 'Venda sob' in lista or 'venda sob' in lista or 'venda Sob' in lista:
-                    lista_ = 'Positiva'
-                elif 'Venda Sem' in lista or 'Venda sem' in lista or 'venda sem' in lista or 'venda Sem' in lista:
-                    lista_ = 'Negativa'
+                # Variáveis para armazenar a descrição e o link
+                descricao = None
+                link = None
 
-                conn = psycopg2.connect(
-                    host="localhost",
-                    database="Anvisa",
-                    user="postgres",
-                    password="BateraDeus"
-                )
+                # Percorre as tags <tr> e verifica se contêm a descrição desejada
+                for tr in tr_tags:
+                    td_tags = tr.find_elements(By.TAG_NAME, 'td')
+                    if len(td_tags) >= 2 and 'MG' in td_tags[1].text or len(td_tags) >= 2 and 'MCG' in td_tags[1].text or len(td_tags) >= 2 and 'MG/G' in td_tags[1].text and '165MG/ML' in td_tags[1].text:
+                        descricao = td_tags[1].text
+                        link = tr.find_element(By.XPATH, './/a')
+                        break
 
-                # Criando um cursor
-                cur = conn.cursor()
-
-                if categoria == 'Fitoterápico' or categoria == 'Fitoterapico':
-                    ncm = 30049099
-                else:
-                    # Executando a consulta NCM
-                    cur.execute(
-                        "SELECT ncm FROM public.pa_ncm WHERE substancia = '%s' LIMIT 1;" % (p_ativo))
-                    qrpa = cur.fetchall()
-                    if len(qrpa) == 0:
-                        ncm = None
-                    else:
-                        ncm = qrpa[0][0]
-
-                # Executando a consulta CEST
-                cur.execute(
-                    "SELECT cest FROM public.cest WHERE categoria ILIKE '%s' AND lista ILIKE '%s';" % (cat_, lista_))
-                qrcest = cur.fetchall()
-                if len(qrcest) == 0:
-                    cest = None
-                else:
-                    cest = qrcest[0][0]
+                # Clica no link se encontrado
+                if link:
+                    link.click()
                 try:
-                    cur.execute("INSERT INTO smerp_meds VALUES (%s, %s, %s, %s, %s, %s, %s);",
-                                (registro, nome, p_ativo, categoria, lista, ncm, cest))
-                    conn.commit()
-                except psycopg2.errors.UniqueViolation:
+                    registro = driver.find_element(
+                        By.XPATH, '//*[@id="anvisa"]/div[1]/div[2]/div[2]').text
+                    nome = driver.find_element(
+                        By.XPATH, '//*[@id="anvisa"]/div[1]/div[3]/div[2]').text
+                    p_ativo = driver.find_element(
+                        By.XPATH, '//*[@id="anvisa"]/div[1]/div[9]/div[2]').text
+                    categoria = driver.find_element(
+                        By.XPATH, '//*[@id="anvisa"]/div[1]/div[10]/div[2]').text
+                    lista = driver.find_element(
+                        By.XPATH, '//*[@id="anvisa"]/div[1]/div[25]/div[2]').text
+                    lista_ = ''
+                    cat_ = ''
+
+                    if categoria != 'Novo' and categoria != 'Similar' and categoria != 'Genérico' and categoria != 'Generico' and categoria != "":
+                        cat_ = 'Outros'
+                    else:
+                        cat_ = categoria
+
+                    if 'Venda Sob' in lista or 'Venda sob' in lista or 'venda sob' in lista or 'venda Sob' in lista:
+                        lista_ = 'Positiva'
+                    elif 'Venda Sem' in lista or 'Venda sem' in lista or 'venda sem' in lista or 'venda Sem' in lista:
+                        lista_ = 'Negativa'
+
+                    conn = psycopg2.connect(
+                        host="localhost",
+                        database="Anvisa",
+                        user="postgres",
+                        password="BateraDeus"
+                    )
+
+                    # Criando um cursor
+                    cur = conn.cursor()
+
+                    if categoria == 'Fitoterápico' or categoria == 'Fitoterapico':
+                        ncm = 30049099
+                    else:
+                        # Executando a consulta NCM
+                        cur.execute(
+                            "SELECT ncm FROM public.pa_ncm WHERE substancia = '%s' LIMIT 1;" % (p_ativo))
+                        qrpa = cur.fetchall()
+                        if len(qrpa) == 0:
+                            ncm = None
+                        else:
+                            ncm = qrpa[0][0]
+
+                    # Executando a consulta CEST
+                    cur.execute(
+                        "SELECT cest FROM public.cest WHERE categoria ILIKE '%s' AND lista ILIKE '%s';" % (cat_, lista_))
+                    qrcest = cur.fetchall()
+                    if len(qrcest) == 0:
+                        cest = None
+                    else:
+                        cest = qrcest[0][0]
+                    try:
+                        cur.execute("INSERT INTO smerp_meds VALUES (%s, %s, %s, %s, %s, %s, %s);",
+                                    (registro, nome, p_ativo, categoria, lista, ncm, cest))
+                        conn.commit()
+                    except psycopg2.errors.UniqueViolation:
+                        return jsonify({
+                            'Registro': registro,
+                            'Nome': nome,
+                            'Principio Ativo': p_ativo,
+                            'Categoria': '%s ou %s' % (categoria, cat_),
+                            'Lista': '%s ou %s' % (lista, lista_),
+                            'NCM': ncm,
+                            'CEST': cest
+                        })
+
                     return jsonify({
                         'Registro': registro,
                         'Nome': nome,
@@ -262,15 +274,5 @@ def Pesquisa(med):
                         'CEST': cest
                     })
 
-                return jsonify({
-                    'Registro': registro,
-                    'Nome': nome,
-                    'Principio Ativo': p_ativo,
-                    'Categoria': '%s ou %s' % (categoria, cat_),
-                    'Lista': '%s ou %s' % (lista, lista_),
-                    'NCM': ncm,
-                    'CEST': cest
-                })
-
-            except NoSuchElementException:
-                return 'Item não encontrado'
+                except NoSuchElementException:
+                    return 'Item não encontrado'
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import jsonify,Flask, render_template, request, abort
+from flask import jsonify, Flask, render_template, request, abort
 from flask_cors import CORS
 from lion import Anvisa, Pesquisa
 
 app = Flask(__name__)
 
 CORS(app, origins=['*'])
+
 
 @app.route('/med', methods=['POST'])
 def get_data():
